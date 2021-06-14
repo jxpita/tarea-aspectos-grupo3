c88649176f931d766befe6c54c71998309ba4d3e
milvflor <milvflor@espol.edu.ec>
2021-06-14 01:49:21 -0500

    cambio en el main para usar SceneBuilder

--- Observer/src/application/Main.java
+++ Observer/src/application/Main.java
@@ -1,9 +1,9 @@
 package application;
-	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
@@ -11,14 +11,14 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) {
 		try {
-			AnchorPane root = new AnchorPane();
-			Scene scene = new Scene(root, 600, 400);
+			Parent root = FXMLLoader.load(getClass().getResource("Observer.fxml"));
+			Scene scene = new Scene(root);
+			stage.setTitle("Patrón Observer implementado con AspectJ");
 			stage.setScene(scene);
 			stage.setResizable(false);
-			stage.setTitle("Patrón Observer implementado con AspectJ");
 			stage.show();
 		} catch(Exception ex) {
-			System.out.println(ex.getMessage());
+            System.out.println(ex.getMessage());
 		}
 	}
 	
